8080598a437691b1f494eba48b08ed8e27e83c72
ZeenNattanon <nattanon1452@gmail.com>
2025-03-26 08:06:42 +0700

    first commit

---
+++ src/libs/userLogIn.tsx
@@ -0,0 +1,17 @@
+export default async function userLogIn(userEmail:string, userPassword:string) {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            email: userEmail,
+            password: userPassword 
+        }),
+    })
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch cars")
+    }
+    return await response.json()
+}
